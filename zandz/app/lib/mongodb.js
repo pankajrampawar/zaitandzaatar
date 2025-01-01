b0fd5bab50d37ffda2168240706156618e3b5ed0
pankaj ram pawar <pankajpawars123@gmail.com>
2025-01-02 04:31:45 +0530

    updated mongo locaiton

--- zandz/app/lib/mongodb.js
+++ zandz/app/lib/mongodb.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI; // Add your MongoDB connection string in .env.local
+const uri = process.env.MONGODB_URI;
 let client;
 let clientPromise;
 
