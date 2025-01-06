b760f8e8dc4a4ee2243f1de75ce88bf708505f91
pankaj ram pawar <pankajpawars123@gmail.com>
2025-01-06 09:41:20 +0530

    upated the store front links

--- zandz/app/components/home/heroSection.js
+++ zandz/app/components/home/heroSection.js
@@ -67,13 +67,13 @@ export default function HeroSection() {
 
                             <div className={`${isDrawerOpen ? 'max-h-96' : 'max-h-0'} overflow-hidden bg-white text-2xl absolute top-14 right-0 flex flex-col items-start transition-[max-height] ease-out duration-400 shadow-lg`}>
                                 <button className="py-2 px-4 hover:bg-slate-200 min-w-full flex justify-start">
-                                    <a href="https://www.order.store/store/saint-wich-burgers/FfblFeilXLutIRj0Lu74Kg" target="_blank">DoorDash</a>
+                                    <a href="https://order.online/store/24509526?slug=-11399864&pickup=true&hideModal=true&redirected=true" target="_blank">DoorDash</a>
                                 </button>
                                 <button className="pt-4 pb-2 px-4 hover:bg-slate-200 min-w-full flex justify-start">
-                                    <a href="https://order.online/business/saint-wich-burgers-11755901" target="_blank" className="hover:cursor-pointer">Uber Eats</a>
+                                    <a href="https://www.order.store/store/zait-%26-zaatar/AkhJK0DPUuqNMNVUqNjsfg" target="_blank" className="hover:cursor-pointer">Uber Eats</a>
                                 </button>
                                 <button className="py-2 pb-4 px-4 hover:bg-slate-200 min-w-full flex justify-start">
-                                    <a href="https://saintwichburgers.dine.online" target="_blank">Grubhub</a>
+                                    <a href="https://zaitzaatar.dine.online" target="_blank">Grubhub</a>
                                 </button>
                             </div>
                         </div>
