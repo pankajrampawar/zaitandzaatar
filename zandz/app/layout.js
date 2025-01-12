0521cee11a8af12de7412ef70d97cd33a4480b49
pankaj ram pawar <pankajpawars123@gmail.com>
2025-01-13 01:28:39 +0530

    added google tag manager and google analytics to the site

--- zandz/app/layout.js
+++ zandz/app/layout.js
@@ -11,7 +11,50 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* GOOGLE TAG MANAGER HEAD CODE*/}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NQTK8DC3');
+          `
+          }}
+        >
+        </script>
+        {/* GOOGLE ANALYTICS SCRIPT CODE 1*/}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-E7T0LBR578"
+        >
+        </script>
+
+        {/* GOOGLE ANALYTICS SCRIPT CODE 2*/}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || []; 
+              function gtag(){dataLayer.push(arguments);} 
+              gtag('js', new Date()); 
+              gtag('config', 'G-E7T0LBR578'); 
+            `,
+          }}
+        />
+      </head>
       <body className="bg-background relative">
+
+        {/* GOOGLE TAG MANAGER HEAD CODE*/}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NQTK8DC3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {/* Background layer */}
         <div
           className="absolute inset-0 w-full z-0"
